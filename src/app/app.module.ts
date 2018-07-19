abd98ab6e2322e5f90ebbf2a2ba96620b6994641
vega2k <vega2k@naver.com>
2018-07-19 12:54:54 +0900

    angular user form example first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { ReactiveFormComponent } from './reactive-form.component';
+import { TemplateDrivenFormComponent } from './template-driven-form.component';
+import { UserService } from './user-service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+      BrowserModule,
+      FormsModule,
+      ReactiveFormsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+      AppComponent,
+      ReactiveFormComponent,
+      TemplateDrivenFormComponent	  
+  ],
+  providers: [
+      UserService
+  ],
+  bootstrap: [
+      AppComponent
+  ]
 })
 export class AppModule { }
---
+++ src/app/reactive-form.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormBuilder, Validators } from '@angular/forms';
+import { UserService } from './user-service';
+import { User } from './user';
+
+@Component({
+   selector: 'app-reactive',
+   templateUrl: 'reactive-form.component.html'
+})
+export class ReactiveFormComponent implements OnInit {
+  unamePattern = '^[a-z0-9_-]{8,15}$';
+  pwdPattern = '^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).{6,12}$';
+  mobnumPattern = '^((\\+91-?)|0)?[0-9]{10}$';
+  emailPattern = '^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$';
+
+  isValidFormSubmitted = null;
+
+  userForm = this.formBuilder.group({
+    username: ['', [Validators.required, Validators.pattern(this.unamePattern)]],
+    password: ['', [Validators.required, Validators.pattern(this.pwdPattern)]],
+    mobileNumber: ['', Validators.pattern(this.mobnumPattern)],
+    email: ['', Validators.pattern(this.emailPattern)],
+  });
+
+  constructor(private formBuilder: FormBuilder, private userService: UserService) {
+  }
+  ngOnInit() {
+  }
+  onFormSubmit() {
+     this.isValidFormSubmitted = false;
+     if (this.userForm.invalid) {
+        return;
+     }
+     this.isValidFormSubmitted = true;
+     const user: User = this.userForm.value;
+     this.userService.createUser(user);
+     this.userForm.reset();
+  }
+  get username() {
+     return this.userForm.get('username');
+  }
+  get password() {
+     return this.userForm.get('password');
+  }
+  get mobileNumber() {
+     return this.userForm.get('mobileNumber');
+  }
+  get email() {
+     return this.userForm.get('email');
+  }
+}
